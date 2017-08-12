33f0618abdc3dd4a42cfdfecad2ad7a90efff5ff
sangcomz <sangcomz@naver.com>
2017-08-13 04:24:25 +0900

    add codeviewer

--- app/src/main/kotlin/com/kodesmell/kodesmell/adapter/KodeListAdapter.kt
+++ app/src/main/kotlin/com/kodesmell/kodesmell/adapter/KodeListAdapter.kt
@@ -1,5 +1,6 @@
 package com.kodesmell.kodesmell.adapter
 
+import android.content.Context
 import android.support.v7.widget.AppCompatTextView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -7,6 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.kodesmell.kodesmell.R
 import com.kodesmell.kodesmell.model.Kode
+import io.github.kbiakov.codeview.CodeView
+import io.github.kbiakov.codeview.adapters.Options
+import io.github.kbiakov.codeview.highlight.ColorTheme
 
 /**
  * Created by sangcomz on 12/08/2017.
@@ -28,9 +32,28 @@ class KodeListAdapter(val kodes: List<Kode>) : RecyclerView.Adapter<KodeListAdap
     override fun getItemCount(): Int = kodes.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val context: Context = itemView.context
         val txtItemMessage = itemView.findViewById(R.id.txt_item_message) as AppCompatTextView
+        val cvItemCode = itemView.findViewById(R.id.cv_item_code) as CodeView
         fun setItem(kode: Kode) {
             txtItemMessage.text = kode.message
+
+            kode.code?.let {
+
+                cvItemCode.setOptions(Options.Default.get(context)
+                        .withCode(getShortCode(it.lines(), kode.lineNumber))
+                        .withShadows()
+                        .withTheme(ColorTheme.MONOKAI))
+            }
+
+        }
+
+        fun getShortCode(lines: List<String>, lineNumber: Int): String {
+            var result: String = ""
+            for (i in lineNumber - 4..lineNumber + 3) {
+                result += lines[i] +"\n"
+            }
+            return result
         }
     }
 }
--- app/src/main/kotlin/com/kodesmell/kodesmell/view/MainActivity.kt
+++ app/src/main/kotlin/com/kodesmell/kodesmell/view/MainActivity.kt
@@ -4,29 +4,23 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import com.github.salomonbrys.kodein.KodeinInjected
-import com.github.salomonbrys.kodein.KodeinInjector
-import com.github.salomonbrys.kodein.android.appKodein
 import com.kodesmell.kodesmell.R
 import com.kodesmell.kodesmell.adapter.KodeListAdapter
 import com.kodesmell.kodesmell.model.Kode
 import com.kodesmell.kodesmell.viewmodel.MainViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), KodeinInjected {
-    override val injector: KodeinInjector = KodeinInjector()
-
-//    val apiClient: ApolloClient by instance()
-
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        inject(appKodein())
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-
-        viewModel.getKode().subscribe { setView(it) }
+        viewModel
+                .getKode()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { setView(it) }
     }
 
     private fun setView(kodes: List<Kode>) {
